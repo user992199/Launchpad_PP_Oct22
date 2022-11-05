9beff6a7b1a23f47e4f71da6c52bd18cd8fa3241
Lulky <codingblocks@Codings-MacBook-Pro.local>
2022-11-05 18:59:00 +0530

    Fundamentals

---
+++ 30-10/hello.cpp
@@ -0,0 +1,14 @@
+// https://ide.codingblocks.com/
+#include<iostream> // library
+using namespace std; // to use std namespace
+
+/*
+	Multi
+	Line 
+	Comment
+*/
+
+int main(){ // code starts from here
+	cout<<"Hello World"<<endl; // printing hello world
+	return 0; // end of code
+}
